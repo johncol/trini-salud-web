621d381b7976f0093d023519c46c7d9775a3e34f
John <john.19col@gmail.com>
2016-09-25 20:43:35 -0500

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { LoginModule } from './login/login.module';
 import { SharedModule } from './shared/shared.module';
 import { CustomerModule } from './customer/customer.module';
 import { CertificateModule } from './certificate/certificate.module';
+
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 
@@ -18,6 +21,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule,
+    routing,
     SharedModule,
     LoginModule,
     CustomerModule,
---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { CreateCustomerComponent } from './customer/create-customer/create-customer.component';
+import { UploadCertificateComponent } from './certificate/upload-certificate/upload-certificate.component';
+import { SearchCertificateComponent } from './certificate/search-certificate/search-certificate.component';
+import { CertificateResultComponent } from './certificate/certificate-result/certificate-result.component';
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'customer/create', component: CreateCustomerComponent },
+    { path: 'certificate/upload', component: UploadCertificateComponent },
+    { path: 'certificate/search', component: SearchCertificateComponent },
+    { path: 'certificate/:id', component: CertificateResultComponent },
+    { path: '', pathMatch: 'full', redirectTo: '/login' }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
