e5b6124d4c18336f0fb5f62a5b848f4b958bce77
John <john.19col@gmail.com>
2016-12-22 00:28:27 -0500

    Change prod environment to tests one

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   api: 'https://trini-salud-api.herokuapp.com/',
+  // api: 'https://certificados-trini-salud-api.herokuapp.com/',
   log: false
 };
