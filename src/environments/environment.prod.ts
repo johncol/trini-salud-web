cf955c4ea78551aa888d2e98ba29101957f09e9e
John <john.19col@gmail.com>
2017-01-12 13:08:27 -0500

    Generate build for heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://trini-salud-api.herokuapp.com/',
-  // api: 'https://certificados-trini-salud-api.herokuapp.com/',
+  // api: 'https://trini-salud-api.herokuapp.com/', // dev
+  api: 'https://certificados-trini-salud-api.herokuapp.com/', // prod
   log: false
 };
